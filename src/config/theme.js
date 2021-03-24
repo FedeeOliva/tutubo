4203e7ecdad9db82e6d2860af431b1e4a670b4e4
Fedee Oliva <fedeolivalr@gmail.com>
2021-03-24 01:01:46 -0300

    refactor theme

--- src/config/theme.js
+++ src/config/theme.js
@@ -13,17 +13,5 @@ export const theme = {
 		tablet: '@media screen and (min-width: 48em)', /* 768/16 */
 		desktop: '@media screen and (min-width: 62em)' /* 992/16 */
 	},
-
-	padding:{
-		s: '0.5em', /* 8/16 */
-		m: '0.9375em', /* 15/16 */
-		l: '1.5625em' /* 25/16 */
-	},
-
-	margin:{
-		s: '0.5em', /* 8/16 */
-		m: '0.9375em', /* 15/16 */
-		l: '1.5625em' /* 25/16 */
-	},
 	navbarHeight: '50px',
 }
